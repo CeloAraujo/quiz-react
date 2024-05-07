a6453c0ec6d1cebbb0506a55b0826ca6b5535c15
celoaraujo <celoparaujo99@gmail.com>
2024-05-07 16:37:14 -0300

    configurando reducer

--- quiz/src/components/Welcome.jsx
+++ quiz/src/components/Welcome.jsx
@@ -5,15 +5,16 @@ import Quiz from "../img/quiz.svg";
 import "./Welcome.css";
 
 const Welcome = () => {
-  const quizState = useContext(QuizContext);
+    // dispatch é para entrar no quizReducer e executar o switch
+    // quiz state pega valor, dispatch altera
+  const [quizState, dispatch] = useContext(QuizContext);
 
-  console.log(quizState)
 
   return (
     <div id="welcome">
       <h2>Bem-vindo(a)!</h2>
       <p>Clique no botão abaixo para iniciar</p>
-      <button>Começar</button>
+      <button onClick={() => dispatch({type:"change_State"})}>Começar</button>
       <img src={Quiz} alt="início do Quiz" />
     </div>
   );
--- quiz/src/context/quiz.jsx
+++ quiz/src/context/quiz.jsx
@@ -1,9 +1,30 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
+import questions from "../data/questions";
+
+const Stages = ["Start", "Playing", "Over"];
+
+const initalState = {
+  // estágio atual no inicio do jogo
+  gameStage: Stages[0],
+  questions,
+};
+
+// alterar o estado
+const quizReducer = (state, action) => {
+  console.log(state, action);
+
+  switch (action.type) {
+    case "change_State":
+      return state;
+    default:
+      return state;
+  }
+};
 
 export const QuizContext = createContext();
 
 export const QuizProvider = ({ children }) => {
-  const value = { name: "Quiz" };
+  const value = useReducer(quizReducer, initalState);
 
   return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>;
 };
